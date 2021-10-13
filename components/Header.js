c33c88f5c9918f5df905f1ee379d75705bd4f380
Felipe Umpierre <feumpi@gmail.com>
2021-10-13 10:21:38 -0300

    Create logo and header

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,23 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import Logo from "./Logo";
+
 const Header = () => {
-  return <></>;
+  return (
+    <>
+      <AppBar
+        sx={{
+          bgcolor: "primary.dark",
+          display: "flex",
+          justfifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Toolbar>
+          <Logo />
+        </Toolbar>
+      </AppBar>
+    </>
+  );
 };
 
 export default Header;
