a0575604c4724b74c2224ee094ec571191f46503
Felipe Umpierre <feumpi@gmail.com>
2021-10-14 21:16:38 -0300

    Create about cards

---
+++ components/AboutSection.js
@@ -0,0 +1,61 @@
+import {
+  Container,
+  Box,
+  Card,
+  CardContent,
+  Typography,
+  Button,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import {
+  Lightbulb,
+  DeveloperMode,
+  Groups,
+  Forum,
+  Computer,
+  Code,
+  QuestionAnswer,
+  School,
+  Wifi,
+  Close,
+} from "@mui/icons-material";
+
+import AboutCard from "./AboutCard";
+import RequirementsCard from "./RequirementsCard";
+
+const AboutSection = () => {
+  return (
+    <>
+      <Container>
+        <Box
+          sx={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr 1fr",
+            gridGap: "30px",
+            marginTop: "30px",
+          }}
+        >
+          <AboutCard title="Aprenda" icon={Lightbulb}>
+            Aprenda a teoria em nossas aulas ao vivo, todos os sábados de manhã
+            por meio da Twitch.
+          </AboutCard>
+
+          <AboutCard title="Pratique" icon={DeveloperMode}>
+            Resolva os exercícios propostos para praticar o conteúdo durante a
+            semana, todos com correção automática.
+          </AboutCard>
+
+          <AboutCard title="Interaja" icon={Forum}>
+            Tire dúvidas com nossos monitores e interaja com outros alunos
+            usando o nosso canal do Discord.
+          </AboutCard>
+        </Box>
+      </Container>
+    </>
+  );
+};
+
+export default AboutSection;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
 
+import { Box } from "@mui/material";
+import { useTheme } from "@emotion/react";
+
 import HeroSection from "../components/HeroSection";
+import AboutSection from "../components/AboutSection";
 
 export default function Home() {
+  const theme = useTheme();
+
   return (
     <>
       <Head>
@@ -15,7 +21,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <HeroSection />
+      <Box
+        sx={{
+          bgcolor: theme.palette.primary.main,
+          color: "white",
+        }}
+      >
+        <HeroSection />
+
+        <AboutSection />
+      </Box>
     </>
   );
 }
