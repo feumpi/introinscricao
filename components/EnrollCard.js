b01ae059cefa4759dc7bd7871fedb9ed747019b6
Felipe Umpierre <feumpi@gmail.com>
2021-10-17 09:39:55 -0300

    Create enroll card

---
+++ components/EnrollCard.js
@@ -0,0 +1,13 @@
+import { Card, CardContent } from "@mui/material";
+
+const EnrollCard = () => {
+  return (
+    <>
+      <Card>
+        <CardContent>Enroll</CardContent>
+      </Card>
+    </>
+  );
+};
+
+export default EnrollCard;
--- pages/enroll.js
+++ pages/enroll.js
@@ -1,5 +1,25 @@
+import { Paper, Container, Card } from "@mui/material";
+import { useTheme } from "@emotion/react";
+
+import EnrollCard from "../components/EnrollCard";
+
 const Enroll = () => {
-  return <></>;
+  const theme = useTheme();
+  return (
+    <>
+      <Paper
+        sx={{
+          bgcolor: theme.palette.primary.main,
+          borderRadius: 0,
+          paddingY: "100px",
+        }}
+      >
+        <Container>
+          <EnrollCard />
+        </Container>
+      </Paper>
+    </>
+  );
 };
 
 export default Enroll;
