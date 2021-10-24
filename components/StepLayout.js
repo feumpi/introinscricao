2029453810fca9f3a5a91fd05dd3b8970513eb70
Felipe Umpierre <feumpi@gmail.com>
2021-10-24 19:39:27 -0300

    Adjust StepLayout for narrow screens

--- components/StepLayout.js
+++ components/StepLayout.js
@@ -9,18 +9,41 @@ const StepLayout = ({ children }) => {
     <>
       <Box
         sx={{
-          paddingX: "50px",
+          paddingX: {
+            md: "50px",
+            xs: "10px",
+          },
           paddingY: "25px",
           display: "grid",
-          gridTemplateColumns: "1fr 1fr 1fr 1fr",
-          gridTemplateAreas: `
+          gridTemplateColumns: {
+            md: "1fr 1fr 1fr 1fr",
+            xs: "1fr",
+          },
+          gridTemplateAreas: {
+            md: `
             "instructions content content content"
           `,
+            xs: `
+          "instructions"
+          "content"
+        `,
+          },
         }}
       >
         <Box sx={{ gridArea: "instructions" }}>{childrenArray[0]}</Box>
 
-        <Box sx={{ gridArea: "content", paddingLeft: "50px" }}>
+        <Box
+          sx={{
+            gridArea: "content",
+            paddingLeft: {
+              md: "50px",
+              xs: 0,
+            },
+            marginTop: {
+              xs: "50px",
+            },
+          }}
+        >
           {childrenArray[1]}
         </Box>
       </Box>
